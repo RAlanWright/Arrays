1f8199bd21708b5a8f8a3c7cd3561aaa45afe4ed
RAlanWright <31636206+RAlanWright@users.noreply.github.com>
2019-06-04 17:33:32 -0500

    Added a couple arrays, accepting user input and printing out the index for one

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -10,6 +10,31 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("This program will allow you to output a chosen index of an array.");
+
+            int[] luckyNumbers = {4, 8, 15, 16, 23, 42};
+
+                        
+     
+            Console.WriteLine("The array 'luckNumbers' contains {0}", string.Join(", ", luckyNumbers) + ".");
+            Console.Write("What index position would you like to see from this array? ");
+            string index = Console.ReadLine();
+            
+            Console.WriteLine(luckyNumbers[Convert.ToInt32(index)]);
+
+
+            string[] friends = new string[5];
+            friends[0] = "Jim";
+            friends[1] = "Kelly";
+
+
+            //for (int i = 1; i <= friends.Length; i++)
+            //{
+            //    Console.ReadLine(friends.Append<string>);
+            //}
+
+
+            Console.ReadLine();
         }
     }
 }
